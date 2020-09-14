c50617d94283d7a84c4aebce2bf73a706faf8f8a
Corey Peterson <corey.peterson@ucf.edu>
2020-09-14 11:32:09 -0400

    Creator tutorial new screenshots

--- src/components/creator/creator-tutorial.js
+++ src/components/creator/creator-tutorial.js
@@ -16,12 +16,15 @@ const CreatorTutorial = (props) => {
 				<h3>Build Effective Syntax Mastery Tools</h3>
 				<div className='tutorial-content'>
 					<p>Identify syntax elements using the <span className='strong'>Legend</span>. For example, if the phrase is another language, you might use the Legend to identify parts of speech.</p>
-					<img src='assets/img/creator-tutorial-legend.png' alt='creator tutorial' />
+					<img className='right' src='assets/img/creator-tutorial-legend.png' alt='Example screenshot of the token legend.' />
 				</div>
-				<p>Provide <span className='strong'>instructions</span> or a <span className='strong'>prompt</span> for each question.</p>
 				<div className='tutorial-content'>
-					<img src='assets/img/creator-tutorial-phrase.png' alt='creator tutorial' />
+					<img className='left' src='assets/img/creator-tutorial-question.png' alt='Example screenshot of the question text.' />
+					<p>Provide <span className='strong'>instructions</span> or a <span className='strong'>prompt</span> for each question.</p>
+				</div>
+				<div className='tutorial-content'>
 					<p>Assemble a <span className='strong'>sentence</span> or <span className='strong'>phrase</span> by entering each word, phrase, or part of speech - then identify each token by their syntax from the <span className='strong'>Legend.</span></p>
+					<img className='right' src='assets/img/creator-tutorial-phrase.png' alt='Example screenshot of phrase assembly.' />
 				</div>
 				<p>You can optionally enable additional features, like allowing students <span className="strong">additional attempts</span> for specific questions, or create additional <span className="strong">"fake" tokens</span> that will appear alongside the real ones.</p>
 				<button onClick={dismiss}>Let's Go</button>
