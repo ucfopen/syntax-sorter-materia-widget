ce84521cebc52730f893524291db3ecf005f99e0
Weining Li <weining.li@ucf.edu>
2021-01-20 12:05:00 -0500

    + unsort token by dragging

--- src/components/player/token-drawer.js
+++ src/components/player/token-drawer.js
@@ -11,6 +11,37 @@ const TokenDrawer = (props) => {
 		dispatch({type: 'paginate_question_forward'})
 	}
 
+	const handleTokenDragOver = (event) => {
+		// Exits the function if the max number of guesses have been used
+		if (props.attemptLimit > 1 && (props.attemptsUsed >= props.attemptLimit)) return false
+
+		event.preventDefault()
+	}
+
+	const handleTokenDrop = (event) => {
+		event.preventDefault()
+
+		let dropTokenId = event.dataTransfer.getData("tokenId")
+		let dropTokenName = event.dataTransfer.getData("tokenName")
+		let dropTokenType = event.dataTransfer.getData("tokenType")
+		let dropTokenPhraseIndex = event.dataTransfer.getData("tokenPhraseIndex")
+		let dropTokenStatus = event.dataTransfer.getData("tokenStatus")
+		let dropTokenFakeout = (event.dataTransfer.getData("tokenFakeout") == "true") ? true : false
+
+		if (dropTokenStatus == "sorted")
+		{
+			dispatch({type: 'sorted_right_click', payload: {
+				origin: dropTokenStatus,
+				tokenIndex: parseInt(dropTokenPhraseIndex),
+				questionIndex: global.state.currentIndex,
+				fakeout: dropTokenFakeout,
+				legend: dropTokenType,
+				value: dropTokenName,
+				id: dropTokenId
+			}})
+		}
+	}
+
 	const handleCheckAnswer = () => {
 		let item = global.state.items[global.state.currentIndex]
 
@@ -39,7 +70,7 @@ const TokenDrawer = (props) => {
 	}
 
 	const verify = (item) => {
-		
+
 		if (item.sorted.length != item.correctPhrase.length) {
 			return false
 		}
@@ -78,7 +109,7 @@ const TokenDrawer = (props) => {
 
 	switch (props.responseState) {
 		case 'ready':
-			
+
 			if (isLastQuestion && remaining > 0) {
 				currentResponseText = <span className='controls-message'>You have <span className='strong'>{remaining}</span> attempt{remaining > 1 ? 's' : ''} remaining. Select <span className='strong'>Check Answer</span> to check your answer, or select <span className='strong'>Submit</span> at the top-right for scoring.</span>
 			}
@@ -122,7 +153,9 @@ const TokenDrawer = (props) => {
 		<section className={'token-drawer ' +
 			`${(props.phrase?.length == 0) ? 'empty ' : ''}` +
 			`${props.responseState} ` +
-			`${props.hasFakes ? 'has-fakes ' : ''}`}>
+			`${props.hasFakes ? 'has-fakes ' : ''}`}
+			onDragOver={handleTokenDragOver}
+			onDrop={handleTokenDrop}>
 			{tokenList}
 			<section className='response-controls'>
 				<div className='response-message-container'>
