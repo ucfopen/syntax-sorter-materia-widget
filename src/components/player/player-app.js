13a74bc2a7222815c5329c1bc8fcbb37bbf27c54
Michael Ferrara <michael.ferrara@ucf.edu>
2021-01-27 10:35:30 -0500

    Made it so the reads cannot be highlighted.

--- src/components/player/player-app.js
+++ src/components/player/player-app.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import QuestionSelect from './question-select'
 import PhrasePlayer from './phrase-player'
 import PlayerTutorial from './player-tutorial'
@@ -12,17 +12,28 @@ const PlayerApp = (props) => {
 
 	useEffect(() => {
 		if (global.state.requireInit) {
-			dispatch({type: 'init', payload: {
-				qset: props.qset,
-				title: props.title
-			}})
+			dispatch({
+				type: 'init', payload: {
+					qset: props.qset,
+					title: props.title
+				}
+			})
+
+			document.addEventListener("mouseup", mouseUpHandler)
 		}
 	}, [global.state.requireInit])
+
+	// Used to prevent reads from being highlighted then dragged
+	const mouseUpHandler = () => {
+		if (window.getSelection().toString().length > 0) {
+			window.getSelection().removeAllRanges();
+		}
+	}
 
 	const convertSortedForLogging = (sorted) => {
 
 		let response = []
-		for (let i=0;i<sorted.length;i++) {
+		for (let i = 0; i < sorted.length; i++) {
 
 			for (const term of global.state.legend) {
 				if (parseInt(sorted[i].legend) == term.id) var legend = term.name
@@ -42,7 +53,7 @@ const PlayerApp = (props) => {
 		let isEmpty = false
 
 		for (let item of global.state.items) {
-				if (item.sorted.length <= 0) {
+			if (item.sorted.length <= 0) {
 				isEmpty = true
 				break
 			}
@@ -53,7 +64,7 @@ const PlayerApp = (props) => {
 	const handleSubmit = () => {
 
 		if (emptyQuestionCheck() == true) {
-			dispatch({type: 'toggle_warning'})
+			dispatch({ type: 'toggle_warning' })
 			return
 		}
 		else {
@@ -70,16 +81,16 @@ const PlayerApp = (props) => {
 	}
 
 	const toggleTutorial = () => {
-		dispatch({type: 'toggle_tutorial'})
+		dispatch({ type: 'toggle_tutorial' })
 	}
 
 	const questionText = global.state.items[global.state.currentIndex]?.question.length > 0 ? global.state.items[global.state.currentIndex].question : "Drag and drop to arrange the items below in the correct order."
 
 	const legendList = global.state.legend.map((term, index) => {
-		return <span key={index} className='legend-item'><span className='legend-color' style={{background: term.color}}></span>{term.name}</span>
+		return <span key={index} className='legend-item'><span className='legend-color' style={{ background: term.color }}></span>{term.name}</span>
 	})
 
-	return(
+	return (
 		<div className="player-container">
 			<WarningModal submitForScoring={submitForScoring}></WarningModal>
 			<PlayerTutorial></PlayerTutorial>
@@ -93,14 +104,14 @@ const PlayerApp = (props) => {
 				<section className="card question-container">
 					<p>{questionText}</p>
 					<div className={'hint-text ' +
-					`${(
-						global.state.items[global.state.currentIndex]?.attemptsUsed > 0 && 
-						global.state.items[global.state.currentIndex]?.attemptsUsed < global.state.items[global.state.currentIndex]?.attempts &&
-						global.state.items[global.state.currentIndex]?.responseState != 'correct' &&
-						global.state.items[global.state.currentIndex]?.responseState != 'incorrect-no-attempts' &&
-						global.state.items[global.state.currentIndex]?.hint.length > 0) ? 'show' : ''}`}>
+						`${(
+							global.state.items[global.state.currentIndex]?.attemptsUsed > 0 &&
+							global.state.items[global.state.currentIndex]?.attemptsUsed < global.state.items[global.state.currentIndex]?.attempts &&
+							global.state.items[global.state.currentIndex]?.responseState != 'correct' &&
+							global.state.items[global.state.currentIndex]?.responseState != 'incorrect-no-attempts' &&
+							global.state.items[global.state.currentIndex]?.hint.length > 0) ? 'show' : ''}`}>
 						<span className="strong">Hint: </span><span>{global.state.items[global.state.currentIndex]?.hint}</span>
-				</div>
+					</div>
 				</section>
 				<PhrasePlayer
 					phrase={global.state.items[global.state.currentIndex]?.phrase}
